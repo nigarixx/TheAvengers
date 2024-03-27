98e312b844e15bba5b573ce927e35f3cea94f2c3
nigarixx <alaskarovanigar@gmail.com>
2024-03-27 20:54:23 +0400

    Initial Commit

---
+++ TheAvengers/Hero.swift
@@ -0,0 +1,8 @@
+//
+//  Hero.swift
+//  TheAvengers
+//
+//  Created by Nigar Alaskarova on 27.03.24.
+//
+
+import Foundation
---
+++ TheAvengers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheAvengers
+//
+//  Created by Nigar Alaskarova on 27.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
